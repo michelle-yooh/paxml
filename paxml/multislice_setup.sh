61aba4cb68b2542ab099a6ebd612db47de8504be
Michelle Yoo <yooh@google.com>
2023-04-26 19:53:08 -0700

    Update multislice_setup.sh

--- paxml/multislice_setup.sh
+++ paxml/multislice_setup.sh
@@ -1,2 +1,2 @@
 pip install "jax[tpu]" -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
-pip install -r pip_package/requirements.txt
+pip install -r $(dirname $0)/pip_package/requirements.txt
